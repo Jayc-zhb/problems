cc8b8e02870719d84bba7d68f61bc7a52f7737bf
Jayc-zhb <3296125982@qq.com>
2024-09-26 23:02:55 +0800

    提交1

---
+++ problems/A/A1076/gen.py
@@ -0,0 +1,11 @@
+from random import *
+from cyaron import *
+
+CASES = 20
+
+for _t in range(1, CASES + 1):
+    io = IO(f"{_t}.in")
+    # ==============================
+    io.input_writeln();
+    # ==============================
+    io.close()
